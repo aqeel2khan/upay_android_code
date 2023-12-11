36f8d2a43ee3bf8bdcba776a6f25fc584b3e267b
Hp <aqeel2.er.delhi@gmail.com>
2023-12-10 21:25:12 -0800

    raw

--- UpaymentSDK/src/main/java/com/upayment/upaymentsdk/activity/ActivityWeb.java
+++ UpaymentSDK/src/main/java/com/upayment/upaymentsdk/activity/ActivityWeb.java
@@ -135,7 +135,7 @@ public class ActivityWeb  extends AppCompatActivity {
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
        // String urlTemp="https://apiv2api.upayments.com/get-pay-by-samsung?order_id=p8PS2GtfrGodZxD74RNzby9cDfsEP399&track_id=2tab587HaYmxfEf5kJRo0HWNSvOMuON3&transaction_id=2328669&ref_id=15269530&currency=KWD&amount=40.000&success_url=https://upayments.com/en/&failure_url=https://developers.upayments.com/";
-
+// https://apiv2api.upayments.com/get-pay-by-samsung?order_id=o4SH0e022077v41371Vc10O2751Of824&track_id=0252f91u09182192r2050M19260323552223N553&transaction_id=2670134&ref_id=16590634&currency=KWD&amount=0.010&success_url=https://upayments.com/en/&failure_url=https://error.com
 
       // urlLoad = urlTemp;
 
@@ -407,8 +407,9 @@ public class ActivityWeb  extends AppCompatActivity {
             public void run() {
                 Log.i("URL", url);
                 try {
-
-                    if(url.contains("http://dev-apiv2api.upayments.com/get-pay-by-samsung")) {
+//https://apiv2api.upayments.com/get-pay-by-samsung?order_id=o4SH0e022077v41371Vc10O2751Of824&track_id=0252f91u09182192r2050M19260323552223N553&transaction_id=2670134&ref_id=
+// 16590634&currency=KWD&amount=0.010&success_url=https://upayments.com/en/&failure_url=https://error.com
+                    if(url.contains("http://dev-apiv2api.upayments.com/get-pay-by-samsung") || url.contains("https://apiv2api.upayments.com/get-pay-by-samsung")) {
 
                        // launchSamsungapp(url);
                      //   showSamsung1(url);
